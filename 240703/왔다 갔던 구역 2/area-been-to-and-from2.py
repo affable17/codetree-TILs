58894b9c6dc2b5c3368a14c4a97d52f93b7dee3c
petrichor17 <111341298+affable17@users.noreply.github.com>
2024-07-03 21:21:47 +0900

    Title: 왔다 갔던 구역 2, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240703/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240703/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,28 +1,34 @@
-OFFSET = 11
+OFFSET = 100
 
 N = int(input())
-segment = [tuple(input().split()) for _ in range(N)]
-start =0
-end = 0
-arr =[0]*21
+seq =[]
+array = [0]* 2001
+cur =0
 
-#steps 표시해주기
-for xL, xR in segment:
-    start = end 
-    if xR == 'R':
-        end += int(xL)
-    else: #xR == 'L'
-        end -= int(xL)
+for _ in range(N):
+    distance, direction = tuple(input().split())
+    distance = int(distance)
+
+    if direction == 'R':
+        left_sequence = cur
+        right_sequence= cur + distance
+        cur += distance
+    else:
+        left_sequence = cur - distance
+        right_sequence = cur
+        cur -= distance
     
-    new_st = start + OFFSET
-    new_ed = end + OFFSET
-    if new_st > new_ed:
-        new_st, new_ed = new_ed, new_st
-    for i in range(new_st, new_ed):
-        arr[i]+=1            
+    seq.append([left_sequence, right_sequence])   
+
+for x1, x2 in seq:
+    x1 += OFFSET
+    x2 += OFFSET
+
+    for i in range(x1, x2):
+        array[i] += 1
 
 cnt =0
-for j in range(len(arr)):
-    if arr[j]>=2:
-        cnt+=1
+for elem in array:
+    if elem >= 2:
+        cnt +=1
 print(cnt)
