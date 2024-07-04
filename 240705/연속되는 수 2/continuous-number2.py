0945800848e3bd6af1261282524ff2f9d50e7159
petrichor17 <111341298+affable17@users.noreply.github.com>
2024-07-05 00:13:12 +0900

    Title: 연속되는 수 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240705/연속되는 수 2/continuous-number2.py
+++ 240705/연속되는 수 2/continuous-number2.py
@@ -8,12 +8,12 @@ for _ in range(N):
 
 max_num =0 
 arr2 =[]
-cnt = 0
+cnt = 1
 for i in range(N):
     if i==0 or arr[i]!=arr[i-1]: #수열 원소가 바뀐다면
         arr2.append(cnt)
         cnt = 1
     else:
         cnt +=1
-
+    #print(i , cnt)
 print(max(arr2))
