c31f92a99848fa0a662097b9ddba752907675f1c
petrichor17 <111341298+affable17@users.noreply.github.com>
2024-07-05 13:58:59 +0900

    Title: 만나는 그 순간, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240705/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,37 @@
+N, M = map(int, input().split())
+
+A=[0]
+B=[0]
+cur=0
+
+for _ in range(N):
+    x = input().split()
+    d, t = x[0], int(x[1]) #direction #time
+        
+   
+    for i in range(1, t+1):
+        if d=='R':
+            cur += 1     
+            A.append(cur)
+        else:
+            cur -= 1
+            A.append(cur)     
+
+            
+cur2=0
+for _ in range(M):
+    x = input().split()
+    d, t = x[0], int(x[1])
+
+    for i in range(1, t+1):
+        if d=='R':
+            cur2+=1
+            B.append(cur2)
+        else:
+            cur2-=1
+            B.append(cur2)   
+
+for i in range(1, len(A)):
+    if A[i]==B[i]:
+        print(i)
+        break
