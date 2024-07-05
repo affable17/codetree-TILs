070bb405c9658ccea7fa491e07d39050595f092d
petrichor17 <111341298+affable17@users.noreply.github.com>
2024-07-05 14:29:23 +0900

    Title: 벌금은 누구에게, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 240705/벌금은 누구에게/who-will-pay.py
+++ 240705/벌금은 누구에게/who-will-pay.py
@@ -2,9 +2,12 @@ N, M, K = map(int, input().split())
 
 arr = [0]*(N+1)
 
+ans =-1
 for _ in range(M):
     student = int(input())
     arr[student]+=1
     if arr[student] >= K:
-        print(student)
+        ans = student
         break
+
+print(ans)
