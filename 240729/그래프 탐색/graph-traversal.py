122d0625d10a15037dcbbd73a75f5e06a4d20fb1
petrichor17 <111341298+affable17@users.noreply.github.com>
2024-07-29 14:37:29 +0900

    Title: 그래프 탐색, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240729/그래프 탐색/graph-traversal.py
@@ -0,0 +1,28 @@
+N, M = map(int, input().split())
+
+#인접리스트 만들기 (graph) N개의 정점
+graph = [[]for _ in range(N+1)]
+
+#방문 확인
+visited = [False]*(N+1)
+
+vertex_cnt = 0
+
+def dfs(v):
+    global vertex_cnt
+
+    for curr_v in graph[v]:
+        if not visited[curr_v]:
+            visited[curr_v]=True
+            vertex_cnt+=1
+            dfs(v)
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited[1]=True
+dfs(1)
+
+print(vertex_cnt)
